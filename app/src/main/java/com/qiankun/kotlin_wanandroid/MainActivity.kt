e5cce8ce7bab02097e9f5cebad495e6eb5a87c0f
huangqiankun <18671166183@163.com>
2020-06-15 15:26:23 +0800

    Initial commit

---
+++ app/src/main/java/com/qiankun/kotlin_wanandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.qiankun.kotlin_wanandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
